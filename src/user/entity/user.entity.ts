9ad17385547955549f9ab3e8db56c1ace9d4b897
Marlon Faria <marlon.leite@indt.org.br>
2023-10-13 18:24:47 -0400

    feat: add user entity

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  FARMER = 'farmer',
+  INDIVIDUAL_BUYER = 'individual_buyer',
+  BUSINESS_BUYER = 'business_buyer',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  password_hash: string;
+
+  @Column()
+  phone: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.INDIVIDUAL_BUYER,
+  })
+  role: UserRole;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deleted_at: Date;
+}
